8afeeb5c80c883b661a4435ca22c36f4a0cd2534
mohammed-yassine-dabboussi <mohammed-yassine.dabboussi@laplateforme.io>
2022-09-28 14:59:30 +0200

    [Projet shell.exe Job6]

--- Job5/hello_bye.sh
+++ Job5/hello_bye.sh
@@ -2,10 +2,9 @@
 if [ $1 = 'Hello' ]
 then 
 	echo "Bonjour, je suis un script!"
-else if [ $1 = 'Bye' ]
+elif [ $1 = 'Bye' ]
 then
 	echo "Au revevoir et bonne journée"
 else 
 	echo "Je ne comprends pas"
-fi
 fi
